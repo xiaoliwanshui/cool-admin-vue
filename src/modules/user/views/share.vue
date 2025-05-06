fc790c06b1d3d51a2cf2f2eca21f42669f124552
17691002584 <17691002584@163.com>
2025-05-06 23:44:12 +0800

    feat(user): 添加用户相关页面组件
    - 新增用户收藏、下载、喜欢、分享等功能页面
    - 实现了数据表格、搜索、新增、编辑等通用功能
    - 优化了页面布局和交互设计
    
    Signed-off-by: 17691002584 <17691002584@163.com>

---
+++ src/modules/user/views/share.vue
@@ -0,0 +1,130 @@
+<template>
+	<cl-crud ref="Crud">
+		<cl-row>
+			<!-- 刷新按钮 -->
+			<cl-refresh-btn />
+			<!-- 新增按钮 -->
+			<cl-add-btn />
+			<!-- 删除按钮 -->
+			<cl-multi-delete-btn />
+			<cl-flex1 />
+			<!-- 条件搜索 -->
+			<cl-search ref="Search" />
+		</cl-row>
+
+		<cl-row>
+			<!-- 数据表格 -->
+			<cl-table ref="Table" />
+		</cl-row>
+
+		<cl-row>
+			<cl-flex1 />
+			<!-- 分页控件 -->
+			<cl-pagination />
+		</cl-row>
+
+		<!-- 新增、编辑 -->
+		<cl-upsert ref="Upsert" />
+	</cl-crud>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+	name: "user-share",
+});
+
+import { useCrud, useTable, useUpsert, useSearch } from "@cool-vue/crud";
+import { useCool } from "/@/cool";
+import { useI18n } from "vue-i18n";
+
+const { service } = useCool();
+const { t } = useI18n();
+
+// cl-upsert
+const Upsert = useUpsert({
+	items: [
+		{
+			label: t("选择创建用户"),
+			prop: "createUserId",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("选择更新用户"),
+			prop: "updateUserId",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("标题"),
+			prop: "title",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("分类"),
+			prop: "type",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("选择关联"),
+			prop: "associationId",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("封面"),
+			prop: "cover",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+	],
+});
+
+// cl-table
+const Table = useTable({
+	columns: [
+		{ type: "selection" },
+		{ label: t("创建用户ID"), prop: "createUserId", minWidth: 120 },
+		{ label: t("更新用户ID"), prop: "updateUserId", minWidth: 120 },
+		{ label: t("标题"), prop: "title", minWidth: 120 },
+		{ label: t("分类"), prop: "type", minWidth: 120 },
+		{ label: t("关联id"), prop: "associationId", minWidth: 120 },
+		{ label: t("封面"), prop: "cover", minWidth: 120 },
+		{
+			label: t("创建时间"),
+			prop: "createTime",
+			minWidth: 170,
+			sortable: "desc",
+			component: { name: "cl-date-text" },
+		},
+		{
+			label: t("更新时间"),
+			prop: "updateTime",
+			minWidth: 170,
+			sortable: "custom",
+			component: { name: "cl-date-text" },
+		},
+		{ type: "op", buttons: ["edit", "delete"] },
+	],
+});
+
+// cl-search
+const Search = useSearch();
+
+// cl-crud
+const Crud = useCrud(
+	{
+		service: service.user.share,
+	},
+	(app) => {
+		app.refresh();
+	},
+);
+
+// 刷新
+function refresh(params?: any) {
+	Crud.value?.refresh(params);
+}
+</script>

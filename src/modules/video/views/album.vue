a88df4e087c1fdd2fffc59c771e5bdc429fb0f91
17691002584 <17691002584@163.com>
2025-07-01 22:36:19 +0800

    refactor(video): 优化视频模块代码结构和性能
    - 重构了多个组件和服务的代码结构，提高了代码可读性和可维护性
    - 优化了数据查询和处理逻辑，提升了性能
    - 统一了代码格式和命名规范，增加了代码的一致性- 添加了必要的注释和文档，便于理解和后续开发

--- src/modules/video/views/album.vue
+++ src/modules/video/views/album.vue
@@ -2,12 +2,12 @@
 	<cl-crud ref="Crud">
 		<cl-row>
 			<!-- 刷新按钮 -->
-			<cl-refresh-btn/>
+			<cl-refresh-btn />
 			<!-- 新增按钮 -->
-			<cl-add-btn/>
+			<cl-add-btn />
 			<!-- 删除按钮 -->
-			<cl-multi-delete-btn/>
-			<cl-flex1/>
+			<cl-multi-delete-btn />
+			<cl-flex1 />
 			<!-- 关键字搜索 -->
 			<cl-filter label="类型">
 				<cl-select
@@ -18,29 +18,29 @@
 					tree
 				/>
 			</cl-filter>
-			<cl-search-key/>
+			<cl-search-key />
 		</cl-row>
 
 		<cl-row>
 			<!-- 数据表格 -->
-			<cl-table ref="Table"/>
+			<cl-table ref="Table" />
 		</cl-row>
 
 		<cl-row>
-			<cl-flex1/>
+			<cl-flex1 />
 			<!-- 分页控件 -->
-			<cl-pagination/>
+			<cl-pagination />
 		</cl-row>
 
 		<!-- 新增、编辑 -->
-		<cl-upsert ref="Upsert"/>
+		<cl-upsert ref="Upsert" />
 	</cl-crud>
 	<cl-dialog v-model="visible" :before-close="beforeClose" height="auto" title="视频列表">
 		<videos
 			:albumId="albumId"
 			:modelValue="modelValue"
 			style="height: 600px"
-			@update:model-value="(value) => (modelValue = value)"
+			@update:model-value="value => (modelValue = value)"
 		></videos>
 		<template #footer>
 			<el-button @click="visible = false">取消</el-button>
@@ -63,38 +63,34 @@
 </template>
 
 <script lang="ts" name="video-album" setup>
-import {useCrud, useTable, useUpsert} from "@cool-vue/crud";
-import {useCool} from "/@/cool";
-import {ref} from "vue";
-import videos from "/$/video/components/videos.vue";
-import videosAlbum from "/$/video/components/videos-album.vue";
-import _ from "lodash";
-import {useDict} from "/$/dict";
-import { useForm } from "@cool-vue/crud";
-const {service} = useCool();
+import { useCrud, useForm, useTable, useUpsert } from '@cool-vue/crud';
+import { useCool } from '/@/cool';
+import { ref } from 'vue';
+import videos from '/$/video/components/videos.vue';
+import videosAlbum from '/$/video/components/videos-album.vue';
+import _ from 'lodash';
+import { useDict } from '/$/dict';
+
+const { service } = useCool();
 const visible = ref<boolean>(false);
 const modelValue = ref<Array<any>>([]);
 const albumId = ref<number>(0);
 const videoAlbumVisible = ref<boolean>(false);
-const addListForm  = useForm();
-const {dict} = useDict();
-
-
-
-
+const addListForm = useForm();
+const { dict } = useDict();
 
 const Upsert = useUpsert({
 	items: [
 		{
-			label: "标题",
-			prop: "title",
-			component: {name: "el-input", props: {clearable: true}},
+			label: '标题',
+			prop: 'title',
+			component: { name: 'el-input', props: { clearable: true } },
 			required: true
 		},
-		{label: "图片", prop: "surface_plot", component: {name: "cl-upload"}, required: true},
+		{ label: '图片', prop: 'surface_plot', component: { name: 'cl-upload' }, required: true },
 		{
-			label: "分类",
-			prop: "category_id",
+			label: '分类',
+			prop: 'category_id',
 			component: {
 				name: 'el-tree-select',
 				props: {
@@ -104,56 +100,56 @@ const Upsert = useUpsert({
 			}
 		},
 		{
-			label: "日人气",
-			prop: "popularity_day",
-			hook: "number",
-			component: {name: "el-input-number"},
+			label: '日人气',
+			prop: 'popularity_day',
+			hook: 'number',
+			component: { name: 'el-input-number' },
 			required: true,
 			value: _.random(1000, 3000)
 		},
 		{
-			label: "周人气",
-			prop: "popularity_week",
-			hook: "number",
-			component: {name: "el-input-number"},
+			label: '周人气',
+			prop: 'popularity_week',
+			hook: 'number',
+			component: { name: 'el-input-number' },
 			required: true,
 			value: _.random(1000, 3000)
 		},
 		{
-			label: "月人气",
-			prop: "popularity_month",
-			hook: "number",
-			component: {name: "el-input-number"},
+			label: '月人气',
+			prop: 'popularity_month',
+			hook: 'number',
+			component: { name: 'el-input-number' },
 			required: true,
 			value: _.random(1000, 3000)
 		},
 		{
-			label: "总人气",
-			prop: "popularity_sum",
-			hook: "number",
-			component: {name: "el-input-number"},
+			label: '总人气',
+			prop: 'popularity_sum',
+			hook: 'number',
+			component: { name: 'el-input-number' },
 			required: true,
 			value: _.random(3000, 6000)
 		},
 		{
-			label: "排序",
-			prop: "sort",
-			hook: "number",
-			component: {name: "el-input-number"},
+			label: '排序',
+			prop: 'sort',
+			hook: 'number',
+			component: { name: 'el-input-number' },
 			required: true,
 			value: 0
 		},
 		{
-			label: "内容",
-			prop: "introduce",
-			component: {name: "el-input", props: {type: "textarea", rows: 4}}
+			label: '内容',
+			prop: 'introduce',
+			component: { name: 'el-input', props: { type: 'textarea', rows: 4 } }
 		},
 		{
-			label: "状态",
-			prop: "status",
+			label: '状态',
+			prop: 'status',
 			flex: false,
 			component: {
-				name: "cl-switch",
+				name: 'cl-switch',
 				props: {
 					activeValue: 1,
 					inactiveValue: 2
@@ -163,11 +159,11 @@ const Upsert = useUpsert({
 			value: 1
 		},
 		{
-			label: "推荐",
-			prop: "recommend",
+			label: '推荐',
+			prop: 'recommend',
 			flex: false,
 			component: {
-				name: "cl-switch",
+				name: 'cl-switch',
 				props: {
 					activeValue: 1,
 					inactiveValue: 2
@@ -180,36 +176,43 @@ const Upsert = useUpsert({
 });
 
 function addListFormOpen(scope) {
-
 	addListForm.value?.open({
-		title: "批量添加视频",
+		title: '批量添加视频',
 		items: [
 			{
-				label: "视频标题",
-				prop: "title",
+				label: '视频标题',
+				prop: 'title',
 				required: true,
-				component: {name: "el-input", props: {type: "textarea", rows: 4,clearable: true,
-						placeholder: "举例(用英文逗号分割):恐怖游轮 死神来了"}}
-			},
+				component: {
+					name: 'el-input',
+					props: {
+						type: 'textarea',
+						rows: 4,
+						clearable: true,
+						placeholder: '举例(用英文逗号分割):恐怖游轮 死神来了'
+					}
+				}
+			}
 		],
 		on: {
 			submit(data, { close, done }) {
 				//使用,分割
-				const titles = data.title.trim().split(",");
+				const titles = data.title.replace(/，/g, ',').replace(/\s/g, '').split(',');
 				//并校验titles数据可靠性
-				if (titles.length >0) {
+				if (titles.length > 0) {
 					console.info(scope.row.id);
 					service.video.album_video.add_list({
-						id:scope.row.id,titles
-					})
+						id: scope.row.id,
+						titles
+					});
 				}
 
 				console.log(titles);
 				setTimeout(() => {
 					close();
 				}, 1500);
-			},
-		},
+			}
+		}
 	});
 	console.log(scope);
 }
@@ -217,96 +220,98 @@ function addListFormOpen(scope) {
 // cl-table
 const Table = useTable({
 	columns: [
-		{type: "selection"},
-		{label: "ID", prop: "id", minWidth: 140},
-		{label: "标题", prop: "title", minWidth: 140},
+		{ type: 'selection' },
+		{ label: 'ID', prop: 'id', minWidth: 140 },
+		{ label: '标题', prop: 'title', minWidth: 140 },
 		{
-			label: "图片",
-			prop: "surface_plot",
+			label: '图片',
+			prop: 'surface_plot',
 			minWidth: 100,
-			component: {name: "cl-image", props: {size: 60}}
+			component: { name: 'cl-image', props: { size: 60 } }
 		},
 		{
-			label: "类型", prop: "category_id", dict: dict.get('video_category'),
-			dictColor: true, minWidth: 140
+			label: '类型',
+			prop: 'category_id',
+			dict: dict.get('video_category'),
+			dictColor: true,
+			minWidth: 140
 		},
 		{
-			label: "日人气",
-			prop: "popularity_day"
+			label: '日人气',
+			prop: 'popularity_day'
 		},
 		{
-			label: "周人气",
-			prop: "popularity_week"
+			label: '周人气',
+			prop: 'popularity_week'
 		},
 		{
-			label: "月人气",
-			prop: "popularity_month"
+			label: '月人气',
+			prop: 'popularity_month'
 		},
 		{
-			label: "总人气",
-			prop: "popularity_sum"
+			label: '总人气',
+			prop: 'popularity_sum'
 		},
-		{label: "排序", prop: "sort", minWidth: 140},
-		{label: "内容", prop: "introduce", showOverflowTooltip: true, minWidth: 200},
+		{ label: '排序', prop: 'sort', minWidth: 140 },
+		{ label: '内容', prop: 'introduce', showOverflowTooltip: true, minWidth: 200 },
 		{
-			label: "状态",
-			prop: "status",
+			label: '状态',
+			prop: 'status',
 			minWidth: 100,
 			component: {
-				name: "cl-switch",
+				name: 'cl-switch',
 				props: {
-					activeValue: "1",
-					inactiveValue: "2"
+					activeValue: '1',
+					inactiveValue: '2'
 				}
 			}
 		},
 		{
-			label: "推荐",
-			prop: "recommend",
+			label: '推荐',
+			prop: 'recommend',
 			minWidth: 140,
 			component: {
-				name: "cl-switch",
+				name: 'cl-switch',
 				props: {
-					activeValue: "1",
-					inactiveValue: "2"
+					activeValue: '1',
+					inactiveValue: '2'
 				}
 			}
 		},
 		{
-			label: "创建时间",
-			prop: "createTime",
+			label: '创建时间',
+			prop: 'createTime',
 			minWidth: 160,
-			component: {name: "cl-date-text"}
+			component: { name: 'cl-date-text' }
 		},
 		{
-			label: "更新时间",
-			prop: "updateTime",
+			label: '更新时间',
+			prop: 'updateTime',
 			minWidth: 160,
-			component: {name: "cl-date-text"}
+			component: { name: 'cl-date-text' }
 		},
 		{
-			type: "op",
+			type: 'op',
 			width: 600,
 			buttons: [
-				"edit",
-				"delete",
+				'edit',
+				'delete',
 				{
-					label: "快速添加",
-					async onClick({scope}) {
-						addListFormOpen(scope)
-
+					label: '快速添加',
+					async onClick({ scope }) {
+						addListFormOpen(scope);
 					}
 				},
 				{
-					label: "绑定数据",
-					async onClick({scope}) {
+					label: '绑定数据',
+					async onClick({ scope }) {
 						albumId.value = scope.row.id;
 						visible.value = !visible.value;
 					}
 				},
 				{
-					label: "数据预览",
-					async onClick({scope}) {
+					label: '数据预览',
+					async onClick({ scope }) {
 						albumId.value = scope.row.id;
 						videoAlbumVisible.value = !videoAlbumVisible.value;
 					}
@@ -321,12 +326,12 @@ const Crud = useCrud(
 	{
 		service: service.video.album
 	},
-	(app) => {
+	app => {
 		app.refresh();
 	}
 );
 
-const beforeClose = (done) => {
+const beforeClose = done => {
 	done();
 	albumId.value = 0;
 	modelValue.value = [];
@@ -334,7 +339,7 @@ const beforeClose = (done) => {
 
 const submit = () => {
 	if (modelValue.value.length > 0 && albumId.value) {
-		modelValue.value.forEach((item) => {
+		modelValue.value.forEach(item => {
 			service.video.album_video.add({
 				videos_id: item.id,
 				album_id: albumId.value
--- src/modules/video/views/swiper.vue
+++ src/modules/video/views/swiper.vue
@@ -2,137 +2,137 @@
 	<cl-crud ref="Crud">
 		<cl-row>
 			<!-- 刷新按钮 -->
-			<cl-refresh-btn/>
+			<cl-refresh-btn />
 			<!-- 新增按钮 -->
-			<cl-add-btn/>
+			<cl-add-btn />
 			<!-- 删除按钮 -->
-			<cl-multi-delete-btn/>
-			<cl-flex1/>
+			<cl-multi-delete-btn />
+			<cl-flex1 />
 			<!-- 条件搜索 -->
-			<cl-search ref="Search"/>
+			<cl-search ref="Search" />
 		</cl-row>
 
 		<cl-row>
 			<!-- 数据表格 -->
-			<cl-table ref="Table"/>
+			<cl-table ref="Table" />
 		</cl-row>
 
 		<cl-row>
-			<cl-flex1/>
+			<cl-flex1 />
 			<!-- 分页控件 -->
-			<cl-pagination/>
+			<cl-pagination />
 		</cl-row>
 
 		<!-- 新增、编辑 -->
-		<cl-upsert ref="Upsert"/>
+		<cl-upsert ref="Upsert" />
 	</cl-crud>
 </template>
 
 <script lang="ts" setup>
-import {useDict} from "/$/dict";
-import {useCrud, useSearch, useTable, useUpsert} from "@cool-vue/crud";
-import {useCool} from "/@/cool";
-import {useI18n} from "vue-i18n";
+import { useDict } from '/$/dict';
+import { useCrud, useSearch, useTable, useUpsert } from '@cool-vue/crud';
+import { useCool } from '/@/cool';
+import { useI18n } from 'vue-i18n';
 
 defineOptions({
-	name: "video-swiper",
+	name: 'video-swiper'
 });
 
-const {dict} = useDict();
-const {service} = useCool();
-const {t} = useI18n();
+const { dict } = useDict();
+const { service } = useCool();
+const { t } = useI18n();
 
 // cl-upsert
 const Upsert = useUpsert({
 	items: [
 		{
-			label: t("标题"),
-			prop: "title",
-			component: {name: "el-input", props: {clearable: true}},
-
+			label: t('标题'),
+			prop: 'title',
+			component: { name: 'el-input', props: { clearable: true } }
 		},
 		{
-			label: t("图片"),
-			prop: "image",
-			component: {name: "el-input", props: {clearable: true}},
-
+			label: t('图片'),
+			prop: 'image',
+			component: { name: 'el-input', props: { clearable: true } }
 		},
 		{
-			label: t("页面"),
-			prop: "path",
-			component: {name: "el-input", props: {clearable: true}},
-
+			label: t('页面'),
+			prop: 'path',
+			component: { name: 'el-input', props: { clearable: true } }
 		},
 		{
-			label: t("选择关联"),
-			prop: "relatedId",
-			component: {name: "el-input", props: {clearable: true}},
-
+			label: t('选择关联'),
+			prop: 'relatedId',
+			component: { name: 'el-input', props: { clearable: true } }
 		},
 		{
-			label: t("类型"),
-			prop: "category",
+			label: t('类型'),
+			prop: 'category',
 			component: {
 				name: 'el-tree-select',
 				props: {
-					data: dict.get('video_category'),
+					data: dict.get('video_category')
 				}
 			}
 		},
 		{
-			label: t("排序"),
-			prop: "sort",
-			component: {name: "el-input", props: {clearable: true}},
-
+			label: t('排序'),
+			prop: 'sort',
+			component: { name: 'el-input', props: { clearable: true } }
 		},
 		{
-			label: t("状态"),
-			prop: "status",
-			component: {name: "el-input", props: {clearable: true}},
+			label: t('状态'),
+			prop: 'status',
+			component: { name: 'el-input', props: { clearable: true } },
 
-			required: true,
-		},
-	],
+			required: true
+		}
+	]
 });
 
 // cl-table
 const Table = useTable({
 	columns: [
-		{type: "selection"},
-		{label: t("标题"), prop: "title", minWidth: 120},
+		{ type: 'selection' },
+		{ label: t('标题'), prop: 'title', minWidth: 120 },
 		{
-			label: t("图片"), prop: "image", minWidth: 120, component: {
-				name: 'el-image',
+			label: t('图片'),
+			prop: 'image',
+			minWidth: 120,
+			component: {
+				name: 'cl-image'
 			}
 		},
 		{
-			label: "分类", prop: "category", dict: dict.get('video_category'),
+			label: '分类',
+			prop: 'category',
+			dict: dict.get('video_category'),
 			dictColor: true,
 			minWidth: 150,
-			dictAllLevels: true, // 显示所有等级
+			dictAllLevels: true // 显示所有等级
 		},
-		{label: t("页面"), prop: "path", minWidth: 120},
-		{label: t("关联ID"), prop: "relatedId", minWidth: 120},
-		{label: t("排序"), prop: "sort", minWidth: 120},
-		{label: t("状态"), prop: "status", minWidth: 120},
-		{label: t("创建用户ID"), prop: "createUserId", minWidth: 120},
-		{label: t("更新用户ID"), prop: "updateUserId", minWidth: 120},
+		{ label: t('页面'), prop: 'path', minWidth: 120 },
+		{ label: t('关联ID'), prop: 'relatedId', minWidth: 120 },
+		{ label: t('排序'), prop: 'sort', minWidth: 120 },
+		{ label: t('状态'), prop: 'status', minWidth: 120 },
+		{ label: t('创建用户ID'), prop: 'createUserId', minWidth: 120 },
+		{ label: t('更新用户ID'), prop: 'updateUserId', minWidth: 120 },
 		{
-			label: t("创建时间"),
-			prop: "createTime",
+			label: t('创建时间'),
+			prop: 'createTime',
 			minWidth: 170,
-			sortable: "desc",
-			component: {name: "cl-date-text"},
+			sortable: 'desc',
+			component: { name: 'cl-date-text' }
 		},
 		{
-			label: t("更新时间"),
-			prop: "updateTime",
+			label: t('更新时间'),
+			prop: 'updateTime',
 			minWidth: 170,
-			sortable: "custom",
-			component: {name: "cl-date-text"},
+			sortable: 'custom',
+			component: { name: 'cl-date-text' }
 		},
-		{type: "op", buttons: ["edit", "delete"]},
-	],
+		{ type: 'op', buttons: ['edit', 'delete'] }
+	]
 });
 
 // cl-search
@@ -141,11 +141,11 @@ const Search = useSearch();
 // cl-crud
 const Crud = useCrud(
 	{
-		service: service.video.swiper,
+		service: service.video.swiper
 	},
-	(app) => {
+	app => {
 		app.refresh();
-	},
+	}
 );
 
 // 刷新
